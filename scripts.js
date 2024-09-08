66fb1bc4909f4a2d5192917b6c70446dfc9aa5da
drob1843 <94759314+drob1843@users.noreply.github.com>
2024-09-08 16:28:01 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+/*
+    App: First JavaScript Application
+    Author: Diego Robles
+    Date: 09/08/24
+*/
+
+// Allow the user to enter their guess
+let num = prompt ('Guess a number between 1 & 10');
+// Check to see if the number is a winner
+if (numb == 5); 
+    // If it is, they got it right
+    {
+        alert('You guess the right number!');
+    } else {
+        // If it isn't, they got it wrong
+        alert('You chose poorly...');
+    }
